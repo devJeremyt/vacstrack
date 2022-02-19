ede8ae8f000ac27aedf2fd17307217ec2538bdfd
Jeremy Trimble <devjeremyt@gmail.com>
2022-02-19 09:13:33 -0500

    Created new employee form page

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Database needs to be implemented first so that we can pull stored users');
 });
+
+/*Form page for creating a new User*/
+router.get('/new', (req,res)=>{
+  res.render('user/new')
+})
+
+router.post('/new', (req, res)=>{
+  res.send('Future comfirmation page, or potentially reload the form for the user to create an additional employee record')
+})
 
 module.exports = router;
