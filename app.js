1badb46fab1e4195e600b9ea18938f4b2b1d5f8a
Jeremy Trimble <devjeremyt@gmail.com>
2022-02-25 21:49:21 -0500

    changed style/header

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(session({
   secret: 'rando secret',
   resave: true,
   saveUninitialized: false,
-  cookie: {maxAge:60000, secure: false}
+  cookie: {maxAge:600000, secure: false}
 }))
 
 app.use(passport.initialize())
