601e9ba53668a8cd9bd815eb89e0202ebd001e47
Jeremy Trimble <devjeremyt@gmail.com>
2022-03-31 17:08:14 -0400

    Added test result page

--- routes/records.js
+++ routes/records.js
@@ -15,10 +15,11 @@ router.get('/pendingApproval', authorize(['HR Representative', 'Administrator'])
 
 router.get('/support/new', (req, res)=> res.render('error', {error: "Page coming soon"}))
 
-router.get('/test/new', (req, res)=> res.render('error', {error: "Page coming soon"}))
+router.get('/test/new', (req, res)=> res.render('record/test/new'))
 
 router.get('/needapproval', authorize(['HR Representative', 'Administrator']), (req, res)=> vaccinesController.getRecordsPendingApproval(req, res))
 
 router.get('/markApproved', authorize(['HR Representative', 'Administrator']), (req, res)=> vaccinesController.markRecordApproved(req, res))
+
 
 module.exports = router
